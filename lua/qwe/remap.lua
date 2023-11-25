5cc5793bf0d7ba96d32862f70e41fa4213fdf4fe
Vitaliy Belikov <vital@qwe.lv>
2023-11-25 19:29:40 +0200

    experiments

--- lua/qwe/remap.lua
+++ lua/qwe/remap.lua
@@ -4,15 +4,14 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
-
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 -- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
@@ -21,7 +20,8 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("i", "<C-b>", "<Nop>")
 
-
 -- Disable mouse
 vim.opt.mouse = ""
 
+-- Autobrace
+-- vim.keymap.set("i", "{<CR>", "{<CR>}<Esc>ko")
