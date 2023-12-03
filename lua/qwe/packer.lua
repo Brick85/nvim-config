40b7bf544c3a3633c2afef3d77d058db3d91f0bd
Vitaliy Belikov <vital@qwe.lv>
2023-12-03 10:07:04 +0200

    h2

--- lua/qwe/packer.lua
+++ lua/qwe/packer.lua
@@ -30,7 +30,11 @@ return require("packer").startup(function(use)
 	})
 
 	use("nvim-treesitter/playground")
-	use("theprimeagen/harpoon")
+	use({
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
 	use("mbbill/undotree")
 	use("tpope/vim-fugitive")
 	-- use("prettier/vim-prettier")
