d384bacc24a856a091c4074589d40bbb8a36d396
Vitaliy Belikov <vital@qwe.lv>
2024-01-07 00:48:18 +0200

    aereal; lsp tink

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -90,50 +90,55 @@ return {
 
             local null_ls = require("null-ls")
 
-
-            null_ls.setup({
-                -- on_attach = function(client, bufnr)
-                -- if client.supports_method("textDocument/formatting") then
-                -- vim.keymap.set("n", "<Leader>f", function()
-                --     vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-                -- end, { buffer = bufnr, desc = "[lsp] format" })
-
-                -- format on save
-                -- vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
-                -- vim.api.nvim_create_autocmd(event, {
-                --     buffer = bufnr,
-                --     group = group,
-                --     callback = function()
-                --         vim.lsp.buf.format({ bufnr = bufnr, async = async })
-                --     end,
-                --     desc = "[lsp] format on save",
-                -- })
-                -- end
-
-                -- if client.supports_method("textDocument/rangeFormatting") then
-                --     vim.keymap.set("x", "<Leader>f", function()
-                --         vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-                --     end, { buffer = bufnr, desc = "[lsp] format" })
-                -- end
-                -- end,
-                -- debug = true,
-            })
-
             local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
             local event = "BufWritePre" -- or "BufWritePost"
             local async = event == "BufWritePost"
-            vim.g.format_on_save = true
-            vim.api.nvim_create_autocmd(event, {
-                group = group,
-                callback = function()
-                    if not vim.g.format_on_save then
-                        return
+
+            null_ls.setup({
+                on_attach = function(client, bufnr)
+                    if client.supports_method("textDocument/formatting") then
+                        -- vim.keymap.set("n", "<Leader>f", function()
+                        vim.keymap.set("n", "<leader>f", function()
+                            vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+                            require("fidget").notify("Formatted null")
+                        end, { buffer = bufnr, desc = "[lsp] format" })
+
+                        -- format on save
+                        vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+                        vim.api.nvim_create_autocmd(event, {
+                            buffer = bufnr,
+                            group = group,
+                            callback = function()
+                                vim.lsp.buf.format({ bufnr = bufnr, async = async })
+                            end,
+                            desc = "[lsp] format on save",
+                        })
                     end
-                    vim.lsp.buf.format({ async = async })
-                    require("fidget").notify("Formatted")
+
+                    if client.supports_method("textDocument/rangeFormatting") then
+                        vim.keymap.set("x", "<Leader>f", function()
+                            vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+                        end, { buffer = bufnr, desc = "[lsp] format" })
+                    end
                 end,
-                desc = "[lsp] format on save",
+                -- debug = true,
             })
+
+            -- local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
+            -- local event = "BufWritePre" -- or "BufWritePost"
+            -- local async = event == "BufWritePost"
+            -- vim.g.format_on_save = true
+            -- vim.api.nvim_create_autocmd(event, {
+            --     group = group,
+            --     callback = function()
+            --         if not vim.g.format_on_save then
+            --             return
+            --         end
+            --         vim.lsp.buf.format({ async = async })
+            --         require("fidget").notify("Formatted")
+            --     end,
+            --     desc = "[lsp] format on save",
+            -- })
 
             lspsettings.setup(lsp_capabilities)
         end,
