ea04dd2f0917c2ecbd0fe5b7e790c7d54076321b
Vitaliy Belikov <vital@qwe.lv>
2024-02-05 15:28:44 +0200

    colors

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -36,3 +36,25 @@ return {
         end,
     },
 }
+-- return {
+--     {
+--         "rose-pine/neovim",
+--         name = "rose-pine",
+--         lazy = false,
+--         priority = 1000,
+--         config = function()
+--             require("rose-pine").setup({
+--                 style = "night",
+--                 transparent = true,
+--                 styles = {
+--                     floats = "transparent",
+--                     sidebars = "transparent",
+--                 },
+--                 dim_inactive = true,
+--             })
+--             vim.cmd([[colorscheme rose-pine]])
+--             -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+--             -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+--         end,
+--     },
+-- }
