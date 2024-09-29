bb9da71e8dacc93aad1b27e329b027cd0c64d009
Vitaliy Belikov <vital@qwe.lv>
2024-09-29 12:35:54 +0300

    enable stylua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -150,6 +150,7 @@ return {
                     null_ls.builtins.formatting.prettierd,
                     null_ls.builtins.formatting.isort,
                     null_ls.builtins.formatting.black,
+                    null_ls.builtins.formatting.stylua,
                     -- null_ls.builtins.formatting.prettierd.with({
                     --     disabled_filetypes = { "sass", "scss", "css" },
                     -- }),
