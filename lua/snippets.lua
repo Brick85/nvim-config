9a2231126040e05c2f5b474bd8f3836698142172
Vitaliy Belikov <vital@qwe.lv>
2024-10-31 14:32:19 +0200

    snippets

--- lua/snippets.lua
+++ lua/snippets.lua
@@ -21,55 +21,70 @@ local types = require("luasnip.util.types")
 local conds = require("luasnip.extras.conditions")
 local conds_expand = require("luasnip.extras.conditions.expand")
 
-ls.cleanup()
-
+-- ls.cleanup()
 ls.add_snippets("python", {
 	-- ls.add_snippets("all", {
-	s("pprint(", {
-		t({ "from pprint import pprint; pprint(" }),
-		i(1, "data"),
-		t(")  # fmt: skip", ""),
-	}),
+	s(
+		"pprint(data)",
+		fmta(
+			[[
+from pprint import pprint; pprint(<data>)  # fmt: skip
+<finish>]],
+			{ data = i(1, "data"), finish = i(0) }
+		)
+	),
 })
+
 ls.add_snippets("htmldjango", {
 	-- ls.add_snippets("all", {
-	s("{% for", {
-		t({ "{% for " }),
-		i(1, "item"),
-		t(" in "),
-		i(2, "items"),
-		t(" %}"),
-		i(0),
-		t({ "{% endfor %}" }),
-	}),
+	s(
+		"{% for item in items %}",
+		fmta(
+			[[{% for <item> in <items> %}<finish>{% endfor %}]],
+			{ item = i(1, "item"), items = i(2, "items"), finish = i(0) }
+		)
+	),
 	s("{% endfor %}", {
 		t("{% endfor %}"),
 	}),
-	s("{% if", {
-		t({ "{% if " }),
-		i(1, "condition"),
-		t(" %}"),
-		i(0),
-		t({ "{% endif %}" }),
+	s(
+		"{% if condition %}",
+		fmta([[{% if <condition> %}<finish>{% endif %}]], { condition = i(1, "condition"), finish = i(0) })
+	),
+	s("{% else %}", {
+		t("{% else %}"),
 	}),
 	s("{% endif %}", {
 		t("{% endif %}"),
 	}),
-	s("{% block", {
-		t({ "{% block " }),
-		i(1),
-		t(" %}"),
-		i(0),
-		t({ "{% endblock " }),
-		rep(1),
-		t(" %}"),
-	}),
+	s(
+		"{% block <name> %}",
+		fmta(
+			[[{% block <name> %}<finish>{% endblock <name_rep> %}]],
+			{ name = i(1, "name"), name_rep = rep(1), finish = i(0) }
+		)
+	),
 	s("{% endblock %}", {
 		t("{% endblock %}"),
 	}),
-	s("{% extends", {
-		t({ "{% extends " }),
-		i(1),
-		t(" %}"),
-	}),
+	s(
+		"{% extends <template> %}",
+		fmta([[{% extends <template> %}<finish>]], { template = i(1, "template"), finish = i(0) })
+	),
+	s(
+		"{% include '<template>' %}",
+		fmta([[{% include '<template>' %}<finish>]], { template = i(1, "template"), finish = i(0) })
+	),
+	s("{% load <library> %}", fmta([[{% load <library> %}<finish>]], { library = i(1, "library"), finish = i(0) })),
+	s("{% url '<name>' %}", fmta([[{% url '<name>' %}<finish>]], { name = i(1, "name"), finish = i(0) })),
+	s("{% static '<path>' %}", fmta([[{% static '<path>' %}<finish>]], { path = i(1, "path"), finish = i(0) })),
+	s("{% blocktrans %}", fmta([[{% blocktrans %}<finish>{% endblocktrans %}]], { finish = i(0) })),
+	s("{% trans '<text>' %}", fmta([[{% trans '<text>' %}<finish>]], { text = i(1, "text"), finish = i(0) })),
+	s(
+		"{% with <var> as <value> %}",
+		fmta(
+			[[{% with <var> as <value> %}<finish>{% endwith %}]],
+			{ var = i(1, "var"), value = i(2, "value"), finish = i(0) }
+		)
+	),
 })
