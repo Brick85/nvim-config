a42c8e810e3fabd5f81816e65dbdfe592711d19c
Vitaliy Belikov <vital@qwe.lv>
2025-08-08 15:23:46 +0300

    add virtual text setting

--- lua/qwe/scripts.lua
+++ lua/qwe/scripts.lua
@@ -3,12 +3,16 @@ local autocmd = vim.api.nvim_create_autocmd
 local yank_group = augroup("HighlightYank", {})
 
 autocmd("TextYankPost", {
-    group = yank_group,
-    pattern = "*",
-    callback = function()
-        vim.highlight.on_yank({
-            higroup = "IncSearch",
-            timeout = 40,
-        })
-    end,
+	group = yank_group,
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "IncSearch",
+			timeout = 40,
+		})
+	end,
+})
+
+vim.diagnostic.config({
+	virtual_text = true, -- Enable inline errors
 })
